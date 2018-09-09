cea82ec07918d2635195fed5534860c86d59af23
User <User@192.168.1.8>
2018-09-09 13:01:53 +0530

    hello

--- jabong/src/test/java/com/qa/tests/ColorPickerTest.java
+++ jabong/src/test/java/com/qa/tests/ColorPickerTest.java
@@ -12,6 +12,7 @@ import org.testng.annotations.AfterTest;
 public class ColorPickerTest extends TestBase {
   @Test
   public void f() {
+	  System.out.println("hiiiiiiii");
   }
   @BeforeTest
   public void beforeTest() {
