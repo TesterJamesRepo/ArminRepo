092286de8578a5e2ceae35ae2bb08e4d7586e987
testerjames007 <testerjames007@gmail.com>
2018-09-23 17:37:01 +0530

    added color picker test

--- jabong/src/test/java/com/qa/tests/ColorPickerTest.java
+++ jabong/src/test/java/com/qa/tests/ColorPickerTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterTest;
 public class ColorPickerTest extends TestBase {
   @Test
   public void f() {
-	  System.out.println("hihiiihhhh");
+	  System.out.println("hih");
   }
   @BeforeTest
   public void beforeTest() {
